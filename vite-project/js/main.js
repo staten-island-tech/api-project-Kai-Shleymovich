84e6da2e63816450a378583101ec459acc1ba0f5
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-12-06 13:14:49 -0500

    eeerm api linked

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,5 +1,5 @@
-import './style.css'
- const URL = 'https://api.spacexdata.com/v5/launches/latest';
+
+/*  const URL = '"https://www.dnd5eapi.co/api/ability-scores/cha"';
 async function getData(URL){
  try {
   const response = await fetch(URL);
@@ -7,7 +7,7 @@ async function getData(URL){
   if( response.status !=200){
       throw new Error(response.statusText)
   }
-//take response from server
+
   const data = await response.json();
   document.querySelector('h1').textContent = data.content;
   document.querySelector('h2').textContent = data.author;  
@@ -17,4 +17,22 @@ async function getData(URL){
 }
 
 }
-getData(URL);
+getData(URL);  */
+
+function testAPI(apiUrl) {
+  fetch(apiUrl)
+    .then((response) => {
+      if (response.ok) {
+        console.log("API is properly incorporated");
+      } else {
+        console.error("API is not properly incorporated");
+      }
+    })
+    .catch((error) => {
+      console.error("Error occurred while testing the API:", error);
+    });
+}
+
+// Usage example
+const apiUrl = "https://www.dnd5eapi.co/api/ability-scores/cha";
+testAPI(apiUrl);
