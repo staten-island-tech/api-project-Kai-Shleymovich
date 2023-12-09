622fe7c9fa8794fa4f3ab19874875053364b14f2
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-12-08 21:20:26 -0500

    lots o progress

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,32 +1,29 @@
+const nameInput = document.getElementById("monsterName");
+const form = document.getElementById("form");
+const apiUrl = "https://www.dnd5eapi.co/api/monsters";
+const output = document.getElementById("output");
 
-/*  const URL = '"https://www.dnd5eapi.co/api/ability-scores/cha"';
-async function getData(URL){
- try {
-  const response = await fetch(URL);
-  console.log(response);
-  if( response.status !=200){
-      throw new Error(response.statusText)
-  }
-
-  const data = await response.json();
-  document.querySelector('h1').textContent = data.content;
-  document.querySelector('h2').textContent = data.author;  
-} catch (error) {
-  documnet.querySelector('h1').textContent = error;
-  documnet.querySelector('h1').textContent = 'please search for something else';
+function condense(str) {
+  return str.split(" ").join("-");
 }
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  console.log(apiUrl + "/" + condense(nameInput.value));
+  const data = await getDataFromAPI(apiUrl + "/" + condense(nameInput.value));
+  console.log(data);
+  const card = makecard(data);
+  output.innerHTML = card;
+});
 
-}
-getData(URL);  */
-
-async function testAPI(apiUrl) {
-  fetch(apiUrl)
-    .then((response) => {
+async function getDataFromAPI(apiUrl) {
+  const res = fetch(apiUrl)
+    .then(async (response) => {
       if (response.ok) {
         console.log("API is properly incorporated");
-        const temp = response.json()
-        console.log(temp)
-        console.log(temp.value.skills)
+        const info = response.json().then((data) => {
+          return data;
+        });
+        return info;
       } else {
         console.error("API is not properly incorporated");
       }
@@ -34,10 +31,34 @@ async function testAPI(apiUrl) {
     .catch((error) => {
       console.error("Error occurred while testing the API:", error);
     });
+  return res;
 }
+function makecard(data) {
+  return `<div class="card">
+  <h2>${data.name}, CR: ${data.challenge_rating}</h2>
+  <h4>${data.size} ${data.type}, ${data.alignment}</h4>
+  <img src="${"https://dnd5eapi.co".concat(data.image)}" alt=""/> 
+  <h4>AC: ${data.armor_class[0].value}</h4>
+  <h4>HP: ${data.hit_points} (${data.hit_points_roll}) </h4>
+  <h4>Speed: ${data.speed.walk}</h4>
+  <ul>
+  <li>STR: ${data.strength}</li>
+  <li>DEX: ${data.dexterity}</li>
+  <li>CON: ${data.constitution}</li>
+  <li>INT: ${data.intelligence}</li>
+  <li>WIS: ${data.wisdom}</li>
+  <li>RIZ: ${data.charisma}</li>
+  </ul>
+</div>`;
+}
+// function insertcard() {
+//   data.selector.insertAdjacentHTML("beforeend", makecard());
+// }
+// insertcard();
+// data.nameinput.value = "";
+// data.hexinput.value = "";
+// data.person_name_input.value = "";
 
 // Usage example
-const apiUrl = "https://www.dnd5eapi.co/api/monsters";
-testAPI(apiUrl);
 
-// wait for function to be done. skills.forEach insertadjacent html
+// wait for function to be done. skills.forEach insert adjacent html
